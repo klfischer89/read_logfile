1f892b54b6256128ee1f691bf3d86b274947ee13
klfischer89 <fischer-k4@web.de>
2025-11-24 15:26:29 +0100

    solved exercises for logfiles

--- logfile.py
+++ logfile.py
@@ -9,4 +9,10 @@ if not os.path.exists("./data/syslog"):
 with open("./data/syslog/syslog", mode="r", encoding="utf-8") as file:
     log = file.read()
 
-    
+log_lines = log.split("\n")    
+
+print(len([line for line in log_lines if line != '']))
+
+print(log.count("[UFW BLOCK]"))
+
+print(log.count("SRC=94.102.51.28"))
