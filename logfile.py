3e9f65fb1b155905005bdb58d1edfa14b2553bea
klfischer89 <fischer-k4@web.de>
2025-11-24 15:05:55 +0100

    added file for extracting logfile and added logfiles

---
+++ logfile.py
@@ -0,0 +1,12 @@
+import os
+import zipfile
+
+if not os.path.exists("./data/syslog"):
+    with zipfile.ZipFile("./data/syslog.zip", "r") as z:
+        z.extractall("./data/syslog")
+    
+    
+with open("./data/syslog/syslog", mode="r", encoding="utf-8") as file:
+    log = file.read()
+
+    
